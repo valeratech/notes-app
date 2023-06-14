7dfdaeb41f19e5e3071dfae7e24087210ecc0bbb
Ryan Valera <valeraryan@gmail.com>
2023-06-13 19:09:52 -0700

    Added the chalk module for log messages

--- apps.js
+++ apps.js
@@ -1,4 +1,8 @@
-const validator = require('validator')
+const chalk = require('chalk')
+
 const getNotes= require('./notes.js')
+
 const msg = getNotes()
 console.log(msg)
+
+console.log(chalk.inverse.bold.green('Success!'))

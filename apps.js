1c39c476b277949d55b58a5cbc004038338e243e
Ryan Valera <valeraryan@gmail.com>
2023-06-15 13:01:44 -0700

    Created title and body option to the add command

--- apps.js
+++ apps.js
@@ -7,10 +7,29 @@ console.log(yargs.version('1.1.0'))
 
 // Create add command
 yargs.command({
+    // command:  string (or array of strings) that executes this command when given on the command line, first string may contain positional args
     command: 'add',
+    // describe: string used as the description for the command in help text, use false for a hidden command
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
+    // builder: object declaring the options the command accepts, or a function accepting and returning a yargs instance
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of text',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    // The parsed positional arguments will be populated and are passed as parameter to the handler function.
+    handler: function (argv) {
+        console.log(`Adding a note!`)
+        console.log(`Title: ${argv.title}`)
+        console.log(`Body: ${argv.body}`)
+        // do something with argv
     }
 })
 
@@ -36,9 +55,9 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
+    handler: function (argv) {
         console.log('Reading a note!')
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()

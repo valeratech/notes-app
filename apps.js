5564876c6556489418988db665de45da2db958f6
Ryan Valera <valeraryan@gmail.com>
2023-06-14 21:55:10 -0700

    Created commands for the note app using yargs

--- apps.js
+++ apps.js
@@ -1,8 +1,44 @@
 const chalk = require('chalk')
-
+const yargs = require('yargs')
 const getNotes= require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+// Customize yargs version
+console.log(yargs.version('1.1.0'))
 
-console.log(chalk.inverse.bold.red('Error!'))
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+console.log(yargs.argv)
